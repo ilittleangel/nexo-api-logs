ea0f70c92ec00b5eafddb9e2c38a6434d5cd3ab8
ilittleangel <angel.rojo.perez@gmail.com>
2019-04-08 19:25:17 +0200

    fix: name and description of setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open('README.md') as f:
     readme = f.read()
 
 setup(
-    name='nexo-ewons',
+    name='nexo-api-logs',
     version='1.0.0',
-    description='Simple project to ingest tags from ewons',
+    description='Rest Api to get the log traces of the local filesystem.',
     long_description=readme,
     author='Angel Rojo',
     author_email='angel.rojo.perez@gmail.com',
-    url='https://github/ilittleangel/nexo-ewons.git',
+    url='https://github/ilittleangel/nexo-api-logs.git',
     packages=find_packages(exclude='tests'),
     include_package_data=True,
     zip_safe=False,
